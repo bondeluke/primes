917c94507928343dcbd3d86272fcd07bb1024f6e
Luke Bonde <bondeluke@gmail.com>
2024-03-28 16:48:40 -0500

    Readme and command line changes

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 I love prime numbers. This repository is dedicated to finding the very quickly.
 - Finding 1,000,000 primes in less than a few seconds ✅
   - Inspired by [Prime Streaming (PG-13)](https://www.codewars.com/kata/5519a584a73e70fa570005f5)
+  - ``C:\Users\Luke\projects\primes> .\generate_primes.exe 1000000
+    The 1,000,000th prime number is 15,485,863
+    Duration: 0.038s
+    ``
 - Finding 50,000,000 primes in less than a few seconds ✅
-  - Inspired by [Prime Streaming (NC-17)](https://www.codewars.com/kata/5519a584a73e70fa570005f5)****
+  - Inspired by [Prime Streaming (NC-17)](https://www.codewars.com/kata/5519a584a73e70fa570005f5)
+  - ``The 1,000,000th prime number is 15,485,863
+    Duration: 0.039s``
 - Finding 100,000,000,000 primes in less than 3 minutes...🔲
   - Inspired by [Find First 100 Billion Primes](https://www.reddit.com/r/learnprogramming/comments/du8bii/find_first_100_billion_primes/)
 - Finding 100,000,000,000 primes in less than 1 minutes... 🔲 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::time::Instant;
 use crate::prime_iterator::{stream};
+use num_format::{Locale, ToFormattedString};
 
 mod prime_iterator;
 mod wheel;
@@ -9,8 +10,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let count: usize = match args.len() {
-        1 => 50_000_000_000,
-        _ => args[1].parse::<usize>()
+        1 => 1_000_000,
+        _ => args[1].parse::<usize>().unwrap()
     };
 
     let start_time = Instant::now();
@@ -18,10 +19,20 @@ fn main() {
     for _ in 1..count {
         prime_iterator.next();
     }
+    let prime = prime_iterator.next().unwrap();
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
 
-    // The 50 millionth prime number is 982451653
-    println!("The {}th prime number is {}", count, prime_iterator.next().unwrap());
+    // The 50,000,000th prime number is 982,451,653
+    println!("The {}{} prime number is {}", count.to_formatted_string(&Locale::en), post_fix(count), prime.to_formatted_string(&Locale::en));
     println!("Duration: {:01}.{:03}s", elapsed_time.as_secs(), elapsed_time.subsec_millis());
+}
+
+fn post_fix(n: usize) -> &'static str {
+    match n % 10 {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th"
+    }
 }
